62636b4826b830849238c7273c1ccf81771fda75
rishikeshk811 <Admin@DESKTOP-B2FLU8A>
2023-12-06 15:07:49 +0530

    new Statement

--- src/test/java/demo2Project/Class5Test.java
+++ src/test/java/demo2Project/Class5Test.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class Class5Test {
 @Test
 public void method5() {
-	System.out.println("this is method 5 ");
-	
+	System.out.println("this is method1 5 ");
+	System.out.println("this is method2 5 ");
 }
 }
